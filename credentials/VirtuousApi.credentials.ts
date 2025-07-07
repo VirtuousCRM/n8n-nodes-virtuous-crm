f6fb99349148dddab04c9253577fe387face7de5
Adam Valverde <adamv@virtuous.org>
2025-07-07 14:23:28 -0700

    adding virtuous auth to credentials

---
+++ credentials/VirtuousApi.credentials.ts
@@ -0,0 +1,44 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class VirtuousApi implements ICredentialType {
+	name = 'virtuousApi';
+	displayName = 'Virtuous API';
+	documentationUrl = 'https://docs.virtuous.org/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Token',
+			name: 'token',
+			type: 'string',
+			default: '',
+			typeOptions: {
+				password: true,
+			}
+		}
+	];
+
+	// This allows the credential to be used by other parts of n8n
+	// stating how this credential is injected as part of the request
+	// An example is the Http Request node that can make generic calls
+	// reusing this credential
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '={{"Bearer " + $credentials.token}}',
+			},
+		},
+	};
+
+	// The block below tells how this credential can be tested
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials?.domain}}',
+			url: '/api/account/currentuser',
+		},
+	};
+}
