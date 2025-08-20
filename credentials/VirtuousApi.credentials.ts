c3b4acd1f82c3d53ebce2c5b65e45e7eb5435609
Jen Gandy <jennifer.gandy@virtuous.org>
2025-08-19 20:25:04 -0700

    updating api creds to test against org endpoint and allow environment switching

--- credentials/VirtuousApi.credentials.ts
+++ credentials/VirtuousApi.credentials.ts
@@ -8,37 +8,62 @@ import {
 export class VirtuousApi implements ICredentialType {
 	name = 'virtuousApi';
 	displayName = 'Virtuous API';
-	documentationUrl = 'https://docs.virtuous.org/';
+	documentationUrl = 'https://docs.virtuoussoftware.com/';
 	properties: INodeProperties[] = [
 		{
-			displayName: 'Token',
-			name: 'token',
+			displayName: 'Environment',
+			name: 'environment',
+			type: 'options',
+			options: [
+				{
+					name: 'Development',
+					value: 'dev',
+					description: 'Development environment (apidevlegacy.virtuoussoftware.com)',
+				},
+				{
+					name: 'QA',
+					value: 'qa',
+					description: 'QA environment (apiqa.virtuoussoftware.com)',
+				},
+			],
+			default: 'dev',
+			description: 'Select the Virtuous API environment to connect to',
+			required: true,
+		},
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
 			type: 'string',
-			default: '',
 			typeOptions: {
 				password: true,
-			}
-		}
+			},
+			default: '',
+			description: 'Your Virtuous API key for the selected environment',
+			required: true,
+		},
 	];
 
 	// This allows the credential to be used by other parts of n8n
 	// stating how this credential is injected as part of the request
 	// An example is the Http Request node that can make generic calls
 	// reusing this credential
+	// Authentication for Virtuous API using Bearer token
 	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
 			headers: {
-				Authorization: '={{"Bearer " + $credentials.token}}',
+				Authorization: '=Bearer {{$credentials.apiKey}}'
 			},
 		},
 	};
 
-	// The block below tells how this credential can be tested
+	// Test the credentials by making a request to the API
 	test: ICredentialTestRequest = {
 		request: {
-			baseURL: '={{$credentials?.domain}}',
-			url: '/api/account/currentuser',
+			// Use expression to dynamically select base URL based on environment
+			baseURL: '={{$credentials.environment === "qa" ? "https://apiqa.virtuoussoftware.com" : "https://apidevlegacy.virtuoussoftware.com"}}',
+			url: '/api/Organization',
+			method: 'GET',
 		},
 	};
 }
